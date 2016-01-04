ddd0d78e48d4c8b08a55753f54b24fb39bf2a4d0
Llexows <alexis_bauvoisdu59@outlook.fr>
2016-01-04 18:33:27 +0100

    Improved event  listeners  system

---
+++ src/fr/llexows/customMobSpawner/listeners/PlayerBreakSpawnerListener.java
@@ -0,0 +1,9 @@
+package fr.llexows.customMobSpawner.listeners;
+
+/**
+ * Created by Llexows.
+ *
+ * @version 0.1
+ */
+public class PlayerBreakSpawnerListener {
+}
---
+++ src/fr/llexows/customMobSpawner/listeners/PlayerOpenGUIListener.java
@@ -0,0 +1,32 @@
+package fr.llexows.customMobSpawner.listeners;
+
+import fr.llexows.customMobSpawner.Core;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+/**
+ * Created by Llexows.
+ * @version 1.0.1
+ */
+public class PlayerOpenGUIListener implements Listener {
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerInteract(PlayerInteractEvent e){
+        final Player player = e.getPlayer();
+        if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
+            Block b = e.getClickedBlock();
+            if(b.getType().equals(Material.MOB_SPAWNER)){
+                Core.getInstance().getSpawnerManager().openSelectionInventory(player, 1);
+                Core.getInstance().getSpawnerManager().getPlayerSpawner().put(player, (CreatureSpawner) b.getState());
+            }
+        }
+    }
+
+}
